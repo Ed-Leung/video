3e2ef3e08c39fef1d368eeca01d51ceb1cf46b8b
Edmond Leung <pixmation@mac.com>
2022-06-21 23:17:46 -0700

    Initial Commit

---
+++ M5L6 Video Player Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M5L6 Video Player Challenge
+//
+//  Created by Edmond Leung on 6/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M5L6 Video Player Challenge/M5L6_Video_Player_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5L6_Video_Player_ChallengeApp.swift
+//  M5L6 Video Player Challenge
+//
+//  Created by Edmond Leung on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct M5L6_Video_Player_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
